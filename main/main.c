b5f54ea0dcda7ed7061b75b264daf5583aec0df2
Nat <nat@cmmc.io>
2016-10-07 16:59:09 +0700

    Webserver example.

--- main/main.c
+++ main/main.c
@@ -5,17 +5,73 @@
 #include "esp_event_loop.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
+#include "freertos/portmacro.h"
+#include "freertos/event_groups.h"
+#include "esp_log.h"
+#include "tcpip_adapter.h"
+static EventGroupHandle_t wifi_event_group;
+const int CONNECTED_BIT = BIT0;
+//static char* TAG = "app_main";
 
-esp_err_t event_handler(void *ctx, system_event_t *event)
+#include "lwip/err.h"
+#include "string.h"
+
+#define LED_BUILTIN 16
+#define delay(ms) (vTaskDelay(ms/portTICK_RATE_MS))
+
+const static char http_html_hdr[] =
+    "HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n";
+const static char http_index_hml[] = "<!DOCTYPE html>"
+      "<html>\n"
+      "<head>\n"
+      "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
+      "  <style type=\"text/css\">\n"
+      "    html, body, iframe { margin: 0; padding: 0; height: 100%; }\n"
+      "    iframe { display: block; width: 100%; border: none; }\n"
+      "  </style>\n"
+      "<title>HELLO ESP32</title>\n"
+      "</head>\n"
+      "<body>\n"
+      "<h1>Hello World, from ESP32!</h1>\n"
+      "</body>\n"
+      "</html>\n";
+
+
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/api.h"
+
+static esp_err_t event_handler(void *ctx, system_event_t *event)
 {
+    switch(event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+        esp_wifi_connect();
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+        xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+        printf("got ip\n");
+        printf("ip: " IPSTR "\n", IP2STR(&event->event_info.got_ip.ip_info.ip));
+        printf("netmask: " IPSTR "\n", IP2STR(&event->event_info.got_ip.ip_info.netmask));
+        printf("gw: " IPSTR "\n", IP2STR(&event->event_info.got_ip.ip_info.gw));
+        printf("\n");
+        fflush(stdout);
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        /* This is a workaround as ESP32 WiFi libs don't currently
+           auto-reassociate. */
+        esp_wifi_connect();
+        xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+        break;
+    default:
+        break;
+    }
     return ESP_OK;
 }
 
-int app_main(void)
+static void initialise_wifi(void)
 {
-    nvs_flash_init();
-    system_init();
     tcpip_adapter_init();
+    wifi_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
@@ -23,23 +79,97 @@ int app_main(void)
     ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
     wifi_config_t sta_config = {
         .sta = {
-            .ssid = "access_point_name",
-            .password = "password",
+            .ssid = "Nat",
+            .password = "123456789",
             .bssid_set = false
         }
     };
+
     ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
     ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
+}
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    int level = 0;
-    while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
-        level = !level;
-        vTaskDelay(300 / portTICK_PERIOD_MS);
+static void
+http_server_netconn_serve(struct netconn *conn)
+{
+  struct netbuf *inbuf;
+  char *buf;
+  u16_t buflen;
+  err_t err;
+
+  /* Read the data from the port, blocking if nothing yet there.
+   We assume the request (the part we care about) is in one netbuf */
+  err = netconn_recv(conn, &inbuf);
+
+  if (err == ERR_OK) {
+    netbuf_data(inbuf, (void**)&buf, &buflen);
+
+    // strncpy(_mBuffer, buf, buflen);
+
+    /* Is this an HTTP GET command? (only check the first 5 chars, since
+    there are other formats for GET, and we're keeping it very simple )*/
+    printf("buffer = %s \n", buf);
+    if (buflen>=5 &&
+        buf[0]=='G' &&
+        buf[1]=='E' &&
+        buf[2]=='T' &&
+        buf[3]==' ' &&
+        buf[4]=='/' ) {
+          printf("buf[5] = %c\n", buf[5]);
+      /* Send the HTML header
+             * subtract 1 from the size, since we dont send the \0 in the string
+             * NETCONN_NOCOPY: our data is const static, so no need to copy it
+       */
+      gpio_pad_select_gpio(LED_BUILTIN);
+      /* Set the GPIO as a push/pull output */
+      gpio_set_direction(LED_BUILTIN, GPIO_MODE_OUTPUT);
+      if(buf[5]=='h'){
+        gpio_set_level(LED_BUILTIN, 0);
+      }
+      if(buf[5]=='l'){
+        gpio_set_level(LED_BUILTIN, 1);
+
+      }
+      netconn_write(conn, http_html_hdr, sizeof(http_html_hdr)-1, NETCONN_NOCOPY);
+
+      /* Send our HTML page */
+      netconn_write(conn, http_index_hml, sizeof(http_index_hml)-1, NETCONN_NOCOPY);
     }
 
+  }
+  /* Close the connection (server closes in HTTP) */
+  netconn_close(conn);
+
+  /* Delete the buffer (netconn_recv gives us ownership,
+   so we have to make sure to deallocate the buffer) */
+  netbuf_delete(inbuf);
+}
+
+static void http_server(void *pvParameters)
+{
+  struct netconn *conn, *newconn;
+  err_t err;
+  conn = netconn_new(NETCONN_TCP);
+  netconn_bind(conn, NULL, 80);
+  netconn_listen(conn);
+  do {
+     err = netconn_accept(conn, &newconn);
+     if (err == ERR_OK) {
+       http_server_netconn_serve(newconn);
+       netconn_delete(newconn);
+     }
+   } while(err == ERR_OK);
+   netconn_close(conn);
+   netconn_delete(conn);
+}
+int app_main(void)
+{
+    nvs_flash_init();
+    system_init();
+    initialise_wifi();
+
+
+    xTaskCreate(&http_server, "http_server", 2048, NULL, 5, NULL);
     return 0;
 }
 
