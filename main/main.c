ac7cf0e3d06ecb4ebe623d7e75c2cd6267dc6f00
Nat <nat@cmmc.io>
2016-10-07 17:41:43 +0700

    Add cJSON.

--- main/main.c
+++ main/main.c
@@ -16,8 +16,11 @@ const int CONNECTED_BIT = BIT0;
 #include "lwip/err.h"
 #include "string.h"
 
+#include "cJSON.h"
+
 #define LED_BUILTIN 16
 #define delay(ms) (vTaskDelay(ms/portTICK_RATE_MS))
+char* json_unformatted;
 
 const static char http_html_hdr[] =
     "HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n";
@@ -121,17 +124,24 @@ http_server_netconn_serve(struct netconn *conn)
              * NETCONN_NOCOPY: our data is const static, so no need to copy it
        */
 
+      netconn_write(conn, http_html_hdr, sizeof(http_html_hdr)-1, NETCONN_NOCOPY);
+
       if(buf[5]=='h') {
         gpio_set_level(LED_BUILTIN, 0);
+        /* Send our HTML page */
+        netconn_write(conn, http_index_hml, sizeof(http_index_hml)-1, NETCONN_NOCOPY);
       }
       else if(buf[5]=='l') {
         gpio_set_level(LED_BUILTIN, 1);
-
+        /* Send our HTML page */
+        netconn_write(conn, http_index_hml, sizeof(http_index_hml)-1, NETCONN_NOCOPY);
       }
-      netconn_write(conn, http_html_hdr, sizeof(http_html_hdr)-1, NETCONN_NOCOPY);
-
-      /* Send our HTML page */
-      netconn_write(conn, http_index_hml, sizeof(http_index_hml)-1, NETCONN_NOCOPY);
+      else if(buf[5]=='j') {
+    	  netconn_write(conn, json_unformatted, strlen(json_unformatted), NETCONN_NOCOPY);
+      }
+      else {
+          netconn_write(conn, http_index_hml, sizeof(http_index_hml)-1, NETCONN_NOCOPY);
+      }
     }
 
   }
@@ -160,16 +170,57 @@ static void http_server(void *pvParameters)
    netconn_close(conn);
    netconn_delete(conn);
 }
+
+
+static void generate_json() {
+	cJSON *root, *info, *d;
+	root = cJSON_CreateObject();
+
+	cJSON_AddItemToObject(root, "d", d = cJSON_CreateObject());
+	cJSON_AddItemToObject(root, "info", info = cJSON_CreateObject());
+
+	cJSON_AddStringToObject(d, "myName", "CMMC-ESP32-NANO");
+	cJSON_AddNumberToObject(d, "temperature", 30.100);
+	cJSON_AddNumberToObject(d, "humidity", 70.123);
+
+	cJSON_AddStringToObject(info, "ssid", "dummy");
+	cJSON_AddNumberToObject(info, "heap", system_get_free_heap_size());
+	cJSON_AddStringToObject(info, "sdk", system_get_sdk_version());
+	cJSON_AddNumberToObject(info, "time", system_get_time());
+
+	while (1) {
+		cJSON_ReplaceItemInObject(info, "heap",
+				cJSON_CreateNumber(system_get_free_heap_size()));
+		cJSON_ReplaceItemInObject(info, "time",
+				cJSON_CreateNumber(system_get_time()));
+		cJSON_ReplaceItemInObject(info, "sdk",
+				cJSON_CreateString(system_get_sdk_version()));
+
+		json_unformatted = cJSON_PrintUnformatted(root);
+		printf("[len = %d]  ", strlen(json_unformatted));
+
+		for (int var = 0; var < strlen(json_unformatted); ++var) {
+			putc(json_unformatted[var], stdout);
+		}
+
+		printf("\n");
+		fflush(stdout);
+		delay(2000);
+		free(json_unformatted);
+	}
+}
+
 int app_main(void)
 {
     nvs_flash_init();
     system_init();
     initialise_wifi();
-    
+
     gpio_pad_select_gpio(LED_BUILTIN);
+
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(LED_BUILTIN, GPIO_MODE_OUTPUT);
-
+    xTaskCreate(&generate_json, "json", 2048, NULL, 5, NULL);
     xTaskCreate(&http_server, "http_server", 2048, NULL, 5, NULL);
     return 0;
 }
